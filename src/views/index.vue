c6ede49b9e7b3e103cb10ffa3555e72cd6a8d94c
eleven7892678@gmail.com <eleven7892678@gmail.com>
2023-04-27 10:15:29 +0800

    修改

--- src/views/index.vue
+++ src/views/index.vue
@@ -726,6 +726,7 @@ export default {
     },
     handleClose() {
       this.dialogVisible = false
+      this.rewardDetail=[]
       if (!this.detail.skip_animei) {
         setTimeout(() => this.elementVisibleCc = true, 300)
       } else {
